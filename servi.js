c9cef38014c95ef14d2675fd693832e94815e071
Carlos Cubides <wuarnerz10@gmail.com>
2023-05-10 18:32:12 -0500

    Se creo el entorno de la carpeta views y ejs y nuestro middleware

--- servi.js
+++ servi.js
@@ -3,13 +3,29 @@ const app = express();
 
 const port = 3000;
 
+app.set('view engine', 'ejs')
+
+app.set('views',__dirname +'/views');
+
+/*Configurando nuestro primer middleware*/
+app.use(express.static(__dirname + "/public"));
+
 app.get("/",(req, res) => {
-    res.send("Pagina de mis productos");
+    res.render("produc", {title
+        : "Mi pagina de productos"});
 });
 
-app.get("/Pasarela",(req, res) => {
-    res.send("Pasarela de pago");
+app.get("/pasarela",(req, res) => {
+    res.render("pasarela", {title
+        :"Esta sera mi pagina de pasarela"});
 });
+
+
+app.use((req, res) => {
+    res.status(404).sendFile(__dirname + "/public/404.html");
+});
+
+
 
 app.listen(port, () => {
     console.log("Servidor  a su servicio en el puerto", port)
